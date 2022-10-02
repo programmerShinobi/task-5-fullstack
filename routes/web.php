38e0293f23329f09537b4fbb77fe8310b89b0780
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-10-02 16:54:57 +0700

    Clean up the syntax in routes

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ use App\Http\Controllers\DatatablesController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Route::middleware(['XssSanitizer'])->group(function () {});
-
 
 Auth::routes();
 
@@ -42,24 +40,3 @@ Route::get('home/{id}/trash', [PostsController::class, 'move_to_trash']);
 Route::resource('posts', PostsController::class);
 
 Route::resource('categories', CategoriesController::class);
-
-
-// Route::get('post', [DatatablesController::class, 'view_post_management']);
-
-// Route::post('get-post', [DatatablesController::class, 'get_post_listed']);
-
-// Route::post('get-post-publish', [DatatablesController::class, 'get_post_listed_publish']);
-
-// Route::post('get-post-draft', [DatatablesController::class, 'get_post_listed_draft']);
-
-// Route::post('get-post-trash', [DatatablesController::class, 'get_post_listed_trash']);
-
-// Route::get('post/{id}/publish', [DatatablesController::class, 'move_to_publish']);
-
-// Route::get('post/{id}/draft', [DatatablesController::class, 'move_to_draft']);
-
-// Route::get('post/{id}/trash', [DatatablesController::class, 'move_to_trash']);
-
-// Route::get('/datatable-example', function () {
-//     return view('datatables-example');
-// });
